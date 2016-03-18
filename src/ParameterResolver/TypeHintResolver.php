e994497abee678819643670796d984f02aac75ba
Felix Becker <felix.b@outlook.com>
2016-03-18 11:07:15 +0100

    Add TypeHintResolver

---
+++ src/ParameterResolver/TypeHintResolver.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Invoker\ParameterResolver;
+
+use Invoker\ParameterResolver\ParameterResolver;
+use ReflectionFunctionAbstract;
+
+/**
+ * Inject entries using type-hints.
+ *
+ * @author Felix Becker <f.becker@outlook.com>
+ */
+class TypeHintResolver implements ParameterResolver
+{
+    public function getParameters(
+        ReflectionFunctionAbstract $reflection,
+        array $providedParameters,
+        array $resolvedParameters
+    ) {
+        $parameters = $reflection->getParameters();
+
+        // Skip parameters already resolved
+        if (! empty($resolvedParameters)) {
+            $parameters = array_diff_key($parameters, $resolvedParameters);
+        }
+
+        foreach ($parameters as $index => $parameter) {
+            $parameterClass = $parameter->getClass();
+
+            if ($parameterClass && array_key_exists($parameterClass->name, $providedParameters)) {
+                $resolvedParameters[$index] = $providedParameters[$parameterClass->name];
+            }
+        }
+
+        return $resolvedParameters;
+    }
+}
