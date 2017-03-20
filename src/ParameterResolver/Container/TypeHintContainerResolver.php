540c27c86f663e20fe39a24cd72fa76cdb21d41a
Matthieu Napoli <matthieu@mnapoli.fr>
2017-03-20 20:28:22 +0100

    Migrate to PSR-11 (#15)

--- src/ParameterResolver/Container/TypeHintContainerResolver.php
+++ src/ParameterResolver/Container/TypeHintContainerResolver.php
@@ -2,8 +2,8 @@
 
 namespace Invoker\ParameterResolver\Container;
 
-use Interop\Container\ContainerInterface;
 use Invoker\ParameterResolver\ParameterResolver;
+use Psr\Container\ContainerInterface;
 use ReflectionFunctionAbstract;
 
 /**
