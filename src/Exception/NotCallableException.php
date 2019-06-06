471bd849ce9e98672224c43121ced288e6847313
Jacob Dreesen <jacob@hdreesen.de>
2019-06-06 23:24:31 +0200

    Clean up NotCallableException a bit

--- src/Exception/NotCallableException.php
+++ src/Exception/NotCallableException.php
@@ -10,7 +10,7 @@ namespace Invoker\Exception;
 class NotCallableException extends InvocationException
 {
     /**
-     * @param string $value
+     * @param mixed $value
      * @param bool $containerEntry
      * @return self
      */
@@ -18,16 +18,14 @@ class NotCallableException extends InvocationException
     {
         if (is_object($value)) {
             $message = sprintf('Instance of %s is not a callable', get_class($value));
-        } elseif (is_array($value) && isset($value[0]) && isset($value[1])) {
+        } elseif (is_array($value) && isset($value[0], $value[1])) {
             $class = is_object($value[0]) ? get_class($value[0]) : $value[0];
             $extra = method_exists($class, '__call') ? ' A __call() method exists but magic methods are not supported.' : '';
             $message = sprintf('%s::%s() is not a callable.%s', $class, $value[1], $extra);
+        } elseif ($containerEntry) {
+            $message = var_export($value, true) . ' is neither a callable nor a valid container entry';
         } else {
-            if ($containerEntry) {
-                $message = var_export($value, true) . ' is neither a callable nor a valid container entry';
-            } else {
-                $message = var_export($value, true) . ' is not a callable';
-            }
+            $message = var_export($value, true) . ' is not a callable';
         }
 
         return new self($message);
